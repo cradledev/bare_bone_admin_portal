ea268fb1f600a9501b9ea5e80aa3a5fe8f0fbabb
seniorstack007 <seniorstack007@gmail.com>
2022-02-08 09:46:52 +0300

    prototype of Frontend

---
+++ client/src/components/App.js
@@ -0,0 +1,10 @@
+// Imports
+import React from "react"
+
+
+// Component
+const App = () => (
+    <h1>React App</h1>
+)
+
+export default App
---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+// Imports
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// App Imports
+import App from './components/App'
+
+// Render App
+ReactDOM.render(<App/>, document.getElementById('root'))
